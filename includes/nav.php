a5158d6777f9ab5db3546e80d09d84f192d3a178
TOAD163 <toad@toadcraft.net>
2019-05-14 09:27:10 +0200

    fixed stuff

--- includes/nav.php
+++ includes/nav.php
@@ -1,9 +1,9 @@
 <div class="row">
           <div class="col-12">
           <!--import van de navbar -->
-              <nav id="navbar" class="navbar navbar-expand-lg navbar-light">
+              <nav id="nav" class="navbar navbar-expand-lg navbar-light">
 <!-- home knop -->
-    <a class="navbar-brand" href="./index.php"><img src="./image/Logo.png" height="100" width="100"></a>
+    <a class="navbar-brand" href="./index.php"><img src="./images/hardlopen.png" height="100" width="100"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
