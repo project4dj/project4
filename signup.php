c7e4b8f19e193dda8b7cfd609f0d9e07cafd3d34
Dennis <dennisluijkenaar@gmail.com>
2019-05-21 11:58:22 +0200

    Created sign-up script

--- signup.php
+++ signup.php
@@ -48,23 +48,23 @@
                     </form>
                 </div>
                 <div class="hidden2" id="hide2">
-                <form>
+                <form action="signupscript.php" method="post">
                     <div class="form-group">
-                        <label for="formGroupExampleInput">Gebruikersnaam</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                        <label for="username">Gebruikersnaam</label>
+                        <input type="text" name="username" class="form-control" id="username" placeholder="Example input">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email Adres</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <label for="email">Email Adres</label>
+                        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Wachtwoord</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <label for="password">Wachtwoord</label>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Herhaal Wachtwoord</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <label for="password2">Herhaal Wachtwoord</label>
+                        <input type="password" name="password2" class="form-control" id="password2" placeholder="Password">
                     </div>
                     <button type="submit" class="btn btn-primary">Registreer!</button>
                     </form>
