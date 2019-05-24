14229743f6af60e20fd938145d7807ea4146c55d
TOAD163 <toad@toadcraft.net>
2019-05-24 09:18:15 +0200

    last merge fixed

--- index.php
+++ index.php
@@ -25,35 +25,6 @@
       <div>
         <?php include("./functionality/pageloader.php"); ?>
       </div>
-      <div class="row">
-            <div class="col-12 col-md-8">
-                <div class="card text-center">
-                    <div class="card-header bg-primary text-white">
-                    Featured
-                    </div>
-                <div class="card-body">
-                    <h5 class="card-title">Over ExerciseMe</h5>
-                    <p class="card-text">Wij hebben deze website gebouwd voor een schoolproject. <br>
-                    De website is bedoelt om mensen te helpen die willen gaan hardlopen of ondersteuning nodig hebben bij het hardlopen.<br>
-                    heb je nog vragen? klik dan snel op de knop hieronder!</p>
-                    <a href="./contact.php" class="btn btn-primary">Stel een vraag!</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="card" style="width: 20rem height: 15rem; ">
-                    <img src="./images/utrmara.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Utrecht Marathon</h5>
-                    <p class="card-text">Zondag 12 Mei 2019 vind in Utrecht voor de 38e keer de Utrecht Marathon plaats! <br>
-                        met afstanden van een kwart-, een halve- en een hele marathon is er voor iedereen wel wat te doen! <br>
-                        Intresse? klik snel op de knop hieronder!
-                    </p>
-                    <a href="https://www.utrechtmarathon.com/" class="btn btn-primary">Naar de website</a>
-                </div>
-                </div>
-            </div>
-        </div> 
 
         </main>
         <div class="row">
