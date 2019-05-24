0c38b1309606b324a9ef936e17ebcf8b61c3ab5c
TOAD163 <toad@toadcraft.net>
2019-05-24 09:57:14 +0200

    fixed the footer and navbarcarousel

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!--import van de social media buttons -->
-<!--Daadwerekelijke code van de social media buttons -->
+<!-- Daadwerekelijke code van de social media buttons -->
+<script src="//platform-api.sharethis.com/js/sharethis.js#property=5ca46d8d9b272f00119abbea&product=inline-follow-buttons"></script>
 <footer>
-    <p>Alle rechten voorbehouden</p>
-    <!-- <div class="sharethis-inline-follow-buttons" id="footer"></div> -->
+    <div class="sharethis-inline-follow-buttons" id="footer"></div>
 </footer>
