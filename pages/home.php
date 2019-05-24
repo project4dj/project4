ab1d08b84f5492d830ff2148af86c7a20ad1e17d
Dennis <dennisluijkenaar@gmail.com>
2019-05-24 09:51:09 +0200

    Fixed footer (credits to Yassine)

--- pages/home.php
+++ pages/home.php
@@ -1,36 +1,36 @@
 <div id="nav" class="row">
-          <div class="col-12">
-          <!-- import van de carousel -->
-              <?php include("./includes/carousel.php"); ?>
-          </div>
-      </div>
+    <div class="col-12">
+        <!-- import van de carousel -->
+        <?php include("./includes/carousel.php"); ?>
+    </div>
+</div>
 
 <div class="row">
-            <div class="col-12 col-md-8">
-                <div class="card text-center">
-                    <div class="card-header bg-primary text-white">
-                    <br>
-                    </div>
-                <div class="card-body">
-                    <h5 class="card-title">Over ExerciseMe</h5>
-                    <p class="card-text">Wij hebben deze website gebouwd voor een schoolproject. <br>
-                    De website is bedoelt om mensen te helpen die willen gaan hardlopen of ondersteuning nodig hebben bij het hardlopen.<br>
-                    heb je nog vragen? klik dan snel op de knop hieronder!</p>
-                    <a href="./index.php?content=contact" class="btn btn-primary">Stel een vraag!</a>
-                </div>
+    <div class="col-12 col-md-8">
+        <div class="card text-center">
+            <div class="card-header bg-primary text-white">
+                <br>
             </div>
+            <div class="card-body">
+                <h5 class="card-title">Over ExerciseMe</h5>
+                <p class="card-text">Wij hebben deze website gebouwd voor een schoolproject. <br>
+                De website is bedoelt om mensen te helpen die willen gaan hardlopen of ondersteuning nodig hebben bij het hardlopen.<br>
+                heb je nog vragen? klik dan snel op de knop hieronder!</p>
+                <a href="./index.php?content=contact" class="btn btn-primary">Stel een vraag!</a>
             </div>
-            <div class="col-12 col-md-4">
-                <div class="card" style="width: 22rem;">
-                    <img src="./images/utrmara.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Utrecht Marathon</h5>
-                    <p class="card-text">Zondag 12 Mei 2019 vind in Utrecht voor de 38e keer de Utrecht Marathon plaats! <br>
+        </div>
+    </div>
+    <div class="col-12 col-md-4">
+        <div class="card" style="position: right;">
+            <img src="./images/utrmara.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Utrecht Marathon</h5>
+                <p class="card-text">Zondag 12 Mei 2019 vind in Utrecht voor de 38e keer de Utrecht Marathon plaats! <br>
                         met afstanden van een kwart-, een halve- en een hele marathon is er voor iedereen wel wat te doen! <br>
                         Intresse? klik snel op de knop hieronder!
-                    </p>
-                    <a href="https://www.utrechtmarathon.com/" class="btn btn-primary">Naar de website</a>
-                </div>
-                </div>
+                </p>
+                <a href="https://www.utrechtmarathon.com/" class="btn btn-primary">Naar de website</a>
             </div>
-        </div> 
+        </div>
+    </div>
+</div>
--- pages/recensies.php
+++ pages/recensies.php
@@ -12,21 +12,20 @@
   </head>
   <body>
       <main class="container">
-      <div class="row">
+        <div class="row">
           <div class="col-12">
             <div class="card text-center">
-                <div class="card-header bg-primary text-white">
+              <div class="card-header bg-primary text-white">
                     Featured
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
-            </div>
-        </div>
-      
+          </div>
+        </div>  
       </main>
 
 
